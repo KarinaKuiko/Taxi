820509766ee36905da6d5a879bc235304aef1049
karina_kuiko <40235341+KarinaKuiko@users.noreply.github.com>
2024-10-07 07:35:07 +0300

    Update Car.java
    delete annotation

--- driver/src/main/java/org/example/driver/entity/Car.java
+++ driver/src/main/java/org/example/driver/entity/Car.java
@@ -24,7 +24,6 @@ public class Car extends AuditingEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String color;
 
     private String brand;
