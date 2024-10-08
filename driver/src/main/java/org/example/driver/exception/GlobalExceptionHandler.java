4c8f1e319f98f1fe3f22ac374081cc45396c235b
karina_kuiko <40235341+KarinaKuiko@users.noreply.github.com>
2024-10-08 09:42:05 +0300

    Update liquibase.properties
    delete global variables from liquibase.properties

--- driver/src/main/java/org/example/driver/exception/GlobalExceptionHandler.java
+++ driver/src/main/java/org/example/driver/exception/GlobalExceptionHandler.java
@@ -1,9 +1,5 @@
 package org.example.driver.exception;
 
-import org.example.driver.exception.car.CarNotFoundException;
-import org.example.driver.exception.car.DuplicatedCarNumberException;
-import org.example.driver.exception.driver.DriverNotFoundException;
-import org.example.driver.exception.driver.DuplicatedDriverEmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,30 +8,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({CarNotFoundException.class})
-    public ResponseEntity<Object> handleCarNotFoundException(CarNotFoundException exception) {
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<Object> handle(BaseException exception) {
         return ResponseEntity.status(exception.getStatus())
                              .body(exception.getMessage());
     }
-
-    @ExceptionHandler({DuplicatedCarNumberException.class})
-    public ResponseEntity<Object> handleDuplicatedCarNumberException(DuplicatedCarNumberException exception) {
-        return ResponseEntity.status(exception.getStatus())
-                             .body(exception.getMessage());
-    }
-
-    @ExceptionHandler({DriverNotFoundException.class})
-    public ResponseEntity<Object> handleDriverNotFoundException(DriverNotFoundException exception) {
-        return ResponseEntity.status(exception.getStatus())
-                             .body(exception.getMessage());
-    }
-
-    @ExceptionHandler({DuplicatedDriverEmailException.class})
-    public ResponseEntity<Object> handleDuplicatedDriverEmailException(DuplicatedDriverEmailException exception) {
-        return ResponseEntity.status(exception.getStatus())
-                             .body(exception.getMessage());
-    }
-
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
