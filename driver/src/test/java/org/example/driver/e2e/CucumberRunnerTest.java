47f9deb9a130869bc52e3202a868db29ce2ad573
karina_kuiko <KarinaIhorevna@gmail.com>
2024-11-20 08:08:22 +0300

    reporting for driver and passenger services

--- driver/src/test/java/org/example/driver/e2e/CucumberRunnerTest.java
+++ driver/src/test/java/org/example/driver/e2e/CucumberRunnerTest.java
@@ -9,7 +9,13 @@ import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/features/driver.feature"},
-        glue={"org.example.driver.e2e"})
+        glue={"org.example.driver.e2e"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/report.html",
+                "json:target/cucumber-reports/report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        })
 @ContextConfiguration(classes = {CucumberSpringConfiguration.class})
 @SpringBootTest
 public class CucumberRunnerTest {
