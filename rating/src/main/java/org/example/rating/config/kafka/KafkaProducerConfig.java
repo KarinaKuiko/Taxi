49ec372ab69797bcd8af750ca00db6efec148113
karina_kuiko <40235341+KarinaKuiko@users.noreply.github.com>
2025-02-17 17:48:22 +0300

    Fix/logging (#24)
    * add trace for kafka
    * add logback.xml with trace and span id

--- rating/src/main/java/org/example/rating/config/kafka/KafkaProducerConfig.java
+++ rating/src/main/java/org/example/rating/config/kafka/KafkaProducerConfig.java
@@ -1,11 +1,13 @@
 package org.example.rating.config.kafka;
 
+import brave.Tracing;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.rating.constants.KafkaConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -32,6 +34,8 @@ public class KafkaProducerConfig {
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        KafkaTemplate<String, Object> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setObservationEnabled(true);
+        return kafkaTemplate;
     }
 }
