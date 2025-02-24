e0cc473ccaf5da102383c46d3a630352d638820a
karina_kuiko <KarinaIhorevna@gmail.com>
2025-02-24 19:52:52 +0300

    add caching via redis

--- rating/src/main/java/org/example/rating/dto/read/RateReadDto.java
+++ rating/src/main/java/org/example/rating/dto/read/RateReadDto.java
@@ -3,6 +3,8 @@ package org.example.rating.dto.read;
 import lombok.Builder;
 import org.example.rating.entity.enumeration.UserType;
 
+import java.io.Serializable;
+
 @Builder
 public record RateReadDto(
         long id,
@@ -11,5 +13,5 @@ public record RateReadDto(
         int rating,
         Long userId,
         UserType userType
-) {
+) implements Serializable {
 }
